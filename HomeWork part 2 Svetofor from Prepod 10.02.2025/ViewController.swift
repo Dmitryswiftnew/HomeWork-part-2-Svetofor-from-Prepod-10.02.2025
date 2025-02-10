a14845f03c860796c735f16b4f010dd2cff3db59
Dmitryswiftnew <vasilevswiftnew@gmail.com>
2025-02-10 14:10:00 +0300

    Initial Commit

---
+++ HomeWork part 2 Svetofor from Prepod 10.02.2025/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork part 2 Svetofor from Prepod 10.02.2025
+//
+//  Created by Dmitry on 10.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
